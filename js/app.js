c6609fae7666779f628ae1a4688e6d9ea4d0721a
Mathieu <goore.csmoviz@gmail.com>
2020-01-22 12:05:36 +0100

    Ajout button back to top

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
   $(".main").onepage_scroll({
     sectionContainer: "section",
     responsiveFallback: 600,
+    animationTime: 800,
     loop: false
   });
 });
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $(".main").moveTo(1);
+}
